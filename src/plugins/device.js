025860aa5c4ab790139c162902b75edaa7d837ac
ozsayag <ozsayag@gmail.com>
2015-02-28 17:54:03 +0200

    adding device docs

--- src/plugins/device.js
+++ src/plugins/device.js
@@ -1,4 +1,28 @@
 angular.module('cordovaData.device', [])
+/**
+ * @ngdoc directive
+ * @name cordovaDevice
+ * @data Device Information
+ * @restrict A
+ *
+ * @description
+ * Fetches the device information.
+ * 
+ * @pluginId org.apache.cordova.device 
+ * @pluginUrl https://github.com/apache/cordova-plugin-device/blob/master/doc/index.md
+ * @pluginInstall 
+ * ```bash
+ * cordova plugin add org.apache.cordova.device
+ * ```
+ * @pluginVersion r0.3.0
+ *
+ * @example
+ * ```html
+ * <div cordova-device>
+ * Device model: {{device.model}}
+ * </div>
+ * ```
+ */
 .directive('cordovaDevice', ['$window', '$document', '$timeout', function($window, $document, $timeout) {
     return {
         restrict: 'A',
