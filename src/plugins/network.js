9329c4df6a8d5c5d08d9295556c22baf7441d4e7
ozsayag <ozsayag@gmail.com>
2015-02-28 17:55:47 +0200

    adding network docs

--- src/plugins/network.js
+++ src/plugins/network.js
@@ -1,4 +1,29 @@
 angular.module('cordovaData.network', [])
+/**
+ * @ngdoc directive
+ * @name cordovaNetwork
+ * @data Current Network
+ * @restrict A
+ *
+ * @description
+ * Fetches the current network available (WIFI, 3G, etc.).
+ * The directive keeps updating the scope on network changes.
+ * 
+ * @pluginId org.apache.cordova.network-information
+ * @pluginUrl https://github.com/apache/cordova-plugin-network-information/blob/master/doc/index.md
+ * @pluginInstall 
+ * ```bash
+ * cordova plugin add org.apache.cordova.network-information
+ * ```
+ * @pluginVersion r0.2.15
+ *
+ * @example
+ * ```html
+ * <div cordova-network>
+ * Current network: {{network}}
+ * </div>
+ * ```
+ */
 .directive('cordovaNetwork', ['$window', '$document', '$timeout', function($window, $document, $timeout) {
     function updateScope(scope) {
         $timeout(function() {
