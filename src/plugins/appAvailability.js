4b91ae971ec10f5e7800dc70a7f05ae2501db0ec
ozsayag <ozsayag@gmail.com>
2015-02-28 17:54:49 +0200

    Adding available apps plugin

---
+++ src/plugins/appAvailability.js
@@ -0,0 +1,93 @@
+angular.module('cordovaData.appAvailability', [])
+/**
+ * @ngdoc directive
+ * @name cordovaAppAvailability
+ * @data Installed Applications
+ * @restrict A
+ *
+ * @description
+ * Checks installed applications
+ * 
+ * @pluginId com.ohh2ahh.plugins.appavailability
+ * @pluginUrl https://github.com/ohh2ahh/AppAvailability
+ * @pluginInstall 
+ * ```bash
+ * cordova plugin add https://github.com/ohh2ahh/AppAvailability.git
+ * ```
+ * @pluginVersion 0.3.1
+ *
+ * @param {expression} cordovaAppAvailability
+ * [Expression](https://docs.angularjs.org/guide/expression) which evaluates to an
+ * object, an array or a string. When string, it is being checked as uri scheme and
+ * result accessible from scope.appAvailability.
+ * When object, it's values are being checked and accessible as object whose keys are the
+ * corresponding keys from the original object.
+ * When array, it's elements are being checked and accessible as array whose elements are the
+ * results in the same order as the original array. see examples.
+ *
+ * @example
+ * ```html
+ * <div cordova-app-availability="'twitter://'">
+ * Twitter is {{appAvailability ? '' : 'not'}} available.
+ * </div>
+ * ```
+ * @example
+ * ```html
+ * <div cordova-app-availability="{twitter: 'twitter://', facebook: 'fb://'}">
+ * Twitter is {{appAvailability.twitter ? '' : 'not'}} available.
+ * Facebook is {{appAvailability.facebook ? '' : 'not'}} available.
+ * </div>
+ * ```
+ * @example
+ * ```html
+ * <div cordova-app-availability="['twitter://', 'fb://']">
+ * Twitter is {{appAvailability[0] ? '' : 'not'}} available.
+ * Facebook is {{appAvailability[1] ? '' : 'not'}} available.
+ * </div>
+ * ```
+ */
+.directive('cordovaAppAvailability', ['$window', '$timeout', function($window, $timeout) {
+    function checkAndUpdate(uri, scope, key) {
+        appAvailability.check(
+            uri,
+            function() {
+                $timeout(function() {
+                    if (key !== undefined) {
+                        scope.appAvailability[key] = true;
+                    } else {
+                        scope.appAvailability = true;
+                    }
+                });
+            },
+            function() {
+                $timeout(function() {
+                    if (key !== undefined) {
+                        scope.appAvailability[key] = false;
+                    } else {
+                        scope.appAvailability = false;
+                    }
+                });
+            }
+        );
+    }
+    
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            document.addEventListener("deviceready", function() {
+                if ($window.appAvailability.check !== undefined) {
+                    scope.$watchCollection(attrs.cordovaAppAvailability, function(apps) {
+                        if (angular.isString(apps)) {
+                            checkAndUpdate(apps, scope);
+                        } else if (angular.isObject(apps) || angular.isArray(apps)){
+                            scope.appAvailability = angular.isObject(apps) ? {} : [];
+                            angular.forEach(apps, function(app, key) {
+                                checkAndUpdate(app, scope, key);
+                            });
+                        }
+                    });
+                }
+            }, false);
+        }
+    };
+}]);
---
+++ test/plugins/appAvailability.spec.js
@@ -0,0 +1,123 @@
+describe("appAvailability", function() {
+    var $compile,
+        $rootScope,
+        $window,
+        $timeout;
+
+    beforeEach(module('cordovaData.appAvailability'));
+
+    beforeEach(inject(function(_$compile_, _$rootScope_, _$window_, _$timeout_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $window = _$window_;
+        $timeout = _$timeout_;
+        
+        $window.appAvailability = {
+            check: function(uri, success, error) {
+                cb = uri === 'twitter://' ? success : error;
+                cb();
+            }
+        }
+    }));
+    
+    it("istalled application check is injected into scope", function() {
+        $compile('<div cordova-app-availability="\'twitter://\'"></div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability).toBe(true);
+    });
+    
+    it("view is updated after injection", function() {
+        var element = $compile('<div cordova-app-availability="\'twitter://\'">{{appAvailability}}</div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect(element.html()).toBe('true');
+    });
+    
+    it("expression evaluates to string", function() {
+        $compile('<div cordova-app-availability="\'twitter://\'"></div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability).toBe(true);
+    });
+    
+    it("expression evaluates to object", function() {
+        $compile('<div cordova-app-availability="{twitter: \'twitter://\'}"></div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability.twitter).toBe(true);
+    });
+    
+    it("expression evaluates to object with multiple properties", function() {
+        $compile('<div cordova-app-availability="{twitter: \'twitter://\', facebook: \'fb://\'}"></div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability.twitter).toBe(true);
+        expect($rootScope.appAvailability.facebook).toBe(false);
+    });
+    
+    it("expression evaluates to array", function() {
+        $compile('<div cordova-app-availability="[\'twitter://\']"></div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability[0]).toBe(true);
+    });
+    
+    it("expression evaluates to array with multiple elements", function() {
+        $compile('<div cordova-app-availability="[\'twitter://\', \'fb://\']"></div>')($rootScope);
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability[0]).toBe(true);
+        expect($rootScope.appAvailability[1]).toBe(false);
+    });
+    
+    it("expression changes", function() {
+        $compile('<div cordova-app-availability="filter"></div>')($rootScope);
+        
+        $rootScope.filter = 'twitter://';
+        
+        document.dispatchEvent(new Event('deviceready'));
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+
+        expect($rootScope.appAvailability).toBe(true);
+        
+        $rootScope.filter = 'fb://';
+        
+        $rootScope.$digest();
+        
+        $timeout.flush();
+        
+        expect($rootScope.appAvailability).toBe(false);
+    });
+});
