4f842cf2e7aec09c0dfb7cc3bd6254994f9ba924
ozsayag <ozsayag@gmail.com>
2015-02-28 17:54:15 +0200

    adding battery docs

--- src/plugins/battery.js
+++ src/plugins/battery.js
@@ -1,4 +1,30 @@
 angular.module('cordovaData.battery', [])
+/**
+ * @ngdoc directive
+ * @name cordovaBattery
+ * @data Current Battery Status
+ * @restrict A
+ *
+ * @description
+ * Fetches the current battery status.
+ * The directive keeps updating the scope on battery changes.
+ * 
+ * @pluginId org.apache.cordova.battery-status
+ * @pluginUrl https://github.com/apache/cordova-plugin-battery-status/blob/master/doc/index.md
+ * @pluginInstall 
+ * ```bash
+ * cordova plugin add org.apache.cordova.battery-status
+ * ```
+ * @pluginVersion r0.2.12
+ *
+ * @example
+ * ```html
+ * <div cordova-battery>
+ * Battery level: {{battery.level}}
+ * Is device charging?: {{battery.isPlugged}}
+ * </div>
+ * ```
+ */
 .directive('cordovaBattery', ['$window', '$timeout', function($window, $timeout) {
     return {
         restrict: 'A',
